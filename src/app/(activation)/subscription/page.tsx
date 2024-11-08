74e5c34c04e110997173d9a9aef7ae6cad6a4537
T-tech26 <taridoutua341@gmail.com>
2024-11-08 17:07:53 +0100

    Implemeting subscription page

--- src/app/(activation)/subscription/page.tsx
+++ src/app/(activation)/subscription/page.tsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { LanguageSwitcher } from '@/components/LanguageSwitcher'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Subscritpion = () => {
   return (
-    <div>Subscritpion</div>
+    <section className='w-full h-screen bg-subscription-bg bg-no-repeat bg-center bg-cover'>
+      <div
+        className='bg-color-60 bg-opacity-30 w-full h-full flex flex-col'
+      >
+        <div className="w-full h-auto px-[15px] md:px-20 pt-7 flex justify-between item-center">
+          <h1 className='text-color-30 text-xl'>LOGO</h1>
+
+          <LanguageSwitcher />
+        </div>
+
+        <div className="flex-1 flex flex-col items-center justify-center gap-5">
+          <Link
+            href='/activation'
+            className='subscription-btn'
+            >
+            Weekly Subscription
+
+            <Image
+              src='/arrow_right_alt.svg'
+              alt='arrow right'
+              width={24}
+              height={24}
+            />
+          </Link>
+
+          <Link
+            href='/activation'
+            className='subscription-btn'
+          >
+            Monthly Subscription
+
+            <Image
+              src='/arrow_right_alt.svg'
+              alt='arrow right'
+              width={24}
+              height={24}
+            />
+          </Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,10 +12,14 @@ const config: Config = {
   		backgroundImage: {
   			'dark-gradient-180deg': 'linear-gradient(180deg, #281A3A 0%, #4E5275 100%)',
   			'dark-gradient-180deg-reverse': 'linear-gradient(180deg, #4E5275 0%, #281A3A 100%)',
+
   			'light-gradient-180deg': 'linear-gradient(180deg, #1089FF 0%, #A2CDF6 100%)',
   			'light-gradient-180deg-reverse': 'linear-gradient(180deg, #A2CDF6 0%, #1089FF 100%)',
+
   			'dark-gradient-135deg': 'linear-gradient(135deg, #281A3A 0%, #4E5275 100%)',
-  			'light-gradient-135deg': 'linear-gradient(135deg, #1089FF 0%, #A2CDF6 100%)'
+  			'light-gradient-135deg': 'linear-gradient(135deg, #1089FF 0%, #A2CDF6 100%)',
+
+			'subscription-bg': `url('/subscription_img.jpg')`,
   		},
   		colors: {
   			background: 'var(--background)',
@@ -28,9 +32,24 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		dropShadow: {
+			'drop-box': '2px 5px 10px rgb(40 26 58 / 50)'
+		},
+		textShadow: {
+			'text-drop': '2px 5px 10px rgb(40 26 58 / 50)'
+		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+	require("tailwindcss-animate"),
+	function ({ addUtilities }: { addUtilities: Function }) {
+		addUtilities({
+		  '.text-shadow-text-drop': {
+			textShadow: '2px 5px 10px rgb(40 26 58 / 50)'
+		  },
+		})
+	  }
+],
 };
 export default config;
