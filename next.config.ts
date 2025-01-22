0cf6f1b457cf95da6de0dd84efc35b9748e051ee
T-tech26 <taridoutua341@gmail.com>
2025-01-22 03:36:03 +0100

    Removed domain redirect rule from next config file

--- next.config.ts
+++ next.config.ts
@@ -13,21 +13,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'greenapuestas.com', // If the domain is mydomain.com
-          },
-        ],
-        destination: 'https://www.greenapuestas.com', // Redirect to www.mydomain.com
-        permanent: true, // Permanent redirect
-      },
-    ];
-  },
 };
 
 export default nextConfig;
